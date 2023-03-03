bc941639dd19ff2961319c28182ef10d8b1e538c
Goodl3oyZ <panudetzx2@gmail.com>
2023-03-03 14:59:23 +0700

    already sed copy slide

--- average.cpp
+++ average.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
+int main(int argc,char* argv[]){
+    int sum=0;
+    for(int i=1;i<argc;i++)
+    {
+        sum+=atoi(argv[i]);
+    }
+    float N=argc-1;
+    float result=sum/N;
+    if(result >0)
+    {
+        cout<<"---------------------------------"<<endl;
+        cout<<"Average of "<<N<<" numbers = "<<result<<endl;
+        cout<<"---------------------------------"<<endl;
+    }
+    else
+        cout<<"Please input numbers to find average.";
+}
