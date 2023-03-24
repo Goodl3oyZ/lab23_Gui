66f0d46df2ea20ac98b849e0200049cf16087e1a
Goodl3oyZ <panudetzx2@gmail.com>
2023-03-24 08:03:16 +0700

    ty a lot  my friend to teach me

--- calculator.cpp
+++ calculator.cpp
@@ -1,19 +1,60 @@
 #include <windows.h>
+#include <stdio.h>
 
+LRESULT CALLBACK WndProc(HWND,UINT , WPARAM ,LPARAM);
 /* This is where all the input to the window goes to */
+char Text1[20];
+char Text2[20];
+HWND Text_Field,Button,Text_Box1,Text_Box2;
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
-		
-		/* Upon destruction, tell the main thread to stop */
-		case WM_DESTROY: {
-			PostQuitMessage(0);
-			break;
+			case WM_CREATE: 
+				Text_Field = CreateWindow("STATIC","   Please input two numbers",WS_VISIBLE|WS_CHILD|WS_BORDER,25,15,200,25,hwnd,NULL,NULL,NULL);
+				Text_Box1 = CreateWindow("EDIT"," ",WS_BORDER|WS_CHILD|WS_VISIBLE,60,70,50,25,hwnd,NULL,NULL,NULL);
+				Text_Box2 = CreateWindow("EDIT"," ",WS_BORDER|WS_CHILD|WS_VISIBLE,60,105,50,25,hwnd,NULL,NULL,NULL);
+				Button = CreateWindow("BUTTON","+",WS_VISIBLE|WS_CHILD|WS_BORDER,150,55,35,25,hwnd,(HMENU) 1,NULL,NULL);
+				Button = CreateWindow("BUTTON","-",WS_VISIBLE|WS_CHILD|WS_BORDER,150,80,35,25,hwnd,(HMENU) 2,NULL,NULL);
+				Button = CreateWindow("BUTTON","*",WS_VISIBLE|WS_CHILD|WS_BORDER,150,105,35,25,hwnd,(HMENU) 3,NULL,NULL);
+				Button = CreateWindow("BUTTON","/",WS_VISIBLE|WS_CHILD|WS_BORDER,150,130,35,25,hwnd,(HMENU) 4,NULL,NULL);
+				break;
+			case WM_COMMAND: // processing calculatorin in math
+				if(LOWORD(wParam)!=0){
+					int gwtstat1 =0,gwtstat2 =0;
+					gwtstat1 = GetWindowText(Text_Box1,&Text1[0],20);
+					gwtstat2 = GetWindowText(Text_Box2,&Text2[0],20);
+				}
+				if(LOWORD(wParam)==1){
+					float re=atof(Text1)+atof(Text2);
+					char t[100];
+					sprintf(t, "%f", re);
+					:: MessageBox(hwnd,t,"Result",MB_OK);
+				}
+				if(LOWORD(wParam)==2){
+					float re=atof(Text1)-atof(Text2);
+					char t[100];
+					sprintf(t, "%f", re);
+					:: MessageBox(hwnd,t,"Result",MB_OK);
+				}
+				if(LOWORD(wParam)==3){
+					float re=atof(Text1)*atof(Text2);
+					char t[100];
+					sprintf(t, "%f", re);
+					:: MessageBox(hwnd,t,"Result",MB_OK);
+				}
+				if(LOWORD(wParam)==4){
+					float re=atof(Text1)/atof(Text2);
+					char t[100];
+					sprintf(t, "%f", re);
+					:: MessageBox(hwnd,t,"Result",MB_OK);
+				}
+				break;			
+			case WM_DESTROY:
+				PostQuitMessage(0);
+				break;
+			default:
+				return DefWindowProc (hwnd,Message,wParam,lParam);
 		}
-		
-		/* All other messages (a lot of them) are processed using default procedures */
-		default:
-			return DefWindowProc(hwnd, Message, wParam, lParam);
-	}
 	return 0;
 }
 
@@ -31,7 +72,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
 	
 	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.hbrBackground = (HBRUSH)(0xE);
 	wc.lpszClassName = "WindowClass";
 	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
 	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
@@ -41,18 +82,21 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+	hwnd = CreateWindowEx(
+		WS_EX_CLIENTEDGE,
+		"WindowClass",
+		"My Calculator",//On top name program
+		WS_VISIBLE|WS_SYSMENU, // fixed size cant up/down size
 		CW_USEDEFAULT, /* x */
 		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
-		NULL,NULL,hInstance,NULL);
+		250, /* width fixed*/ 
+		200, /* height fixed*/
+		NULL, NULL, hInstance, NULL);
 
 	if(hwnd == NULL) {
 		MessageBox(NULL, "Window Creation Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
 		return 0;
 	}
-
 	/*
 		This is the heart of our program where all input is processed and 
 		sent to WndProc. Note that GetMessage blocks code flow until it receives something, so
@@ -63,4 +107,5 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		DispatchMessage(&msg); /* Send it to WndProc */
 	}
 	return msg.wParam;
+
 }
